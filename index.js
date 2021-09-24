815eb4da598dd7bbde60cb08b395cc9dfe9fbd88
Mochamad Saepul Millah <numusai@gmail.com>
2021-09-24 11:58:59 +0700

    update

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-data = {
-  name: 'Deris',
-  salary: 10000000,
-  categories: [
-    {
-      id: 1,
-      name: 'Food & Drink',
-      budget: 3000000,
-    },
-    {
-      id: 2,
-      name: 'Shopping',
-      budget: 1000000,
-    },
-  ],
-  expenses: [
-    {
-      id: 1,
-      name: 'Makan Siang',
-      amount: 20000,
-      categoryId: 1,
-      note: 'Makan Siang di cafe',
-      createdAt: '17/4/2021',
-    },
-    {
-      id: 2,
-      name: 'Makan Malam',
-      amount: 20000,
-      categoryId: 1,
-      note: 'Makan Malam di rumah',
-      createdAt: '17/4/2021',
-    }
-  ]
-}
-
 function loadData(){
   return JSON.parse(localStorage.getItem('dataUser'));
 }
@@ -50,7 +15,7 @@ function initialize(){
         id: 1,
         name: 'Makanan dan minuman',
         amount: 0,
-        budget: 100000
+        budget: 0
       }
       ,{ 
         id: 2,
@@ -105,19 +70,49 @@ function createExpense(){
 
   if (data.expenses.slice(-1)[0]) id = data.expenses.slice(-1)[0].id + 1
 
-  data.expenses.push({
-    id,
-    amount: Number(document.getElementById("amount").value),
-    categoryId: Number(document.getElementById("category").value),
-    note: document.getElementById("notes").value,
-    date: document.getElementById("date").value
-  })
+  if (document.getElementById("amount").value && document.getElementById("category").value && document.getElementById("date").value) {
+    data.expenses.push({
+      id,
+      amount: Number(document.getElementById("amount").value),
+      categoryId: Number(document.getElementById("category").value),
+      note: document.getElementById("notes").value,
+      date: document.getElementById("date").value
+    })
+  }
 
   return saveData(data)
 }
 
-function editExpense(){
+function editExpense(id){
+  const dataUser = loadData()
+  let expense = ''
+  
+  for (let i = 0; i < dataUser.expenses.length; i++) {
+    if (dataUser.expenses[i].id === id) {
+      expense = dataUser.expenses[i]
+    }   
+  }
 
+  document.querySelector('#main-content').remove()
+  document.querySelector('#menu').remove()
+
+  let homeContent = document.createElement("div");
+  homeContent.className = 'pt-5 px-5'
+
+    // <button type="button" class="btn btn-outline-dark float-end" onclick="listCategories()">Close</button>
+  let expenseForm = `
+    <form class="m-auto mt-5">
+      <div class="mb-3">
+        <label for="amount" class="form-label">Amount :</label>
+        <input type="number" class="form-control" id="amount" placeholder="100000" value="${expense.amount}">
+      </div>
+      <button type="submit" class="btn btn-primary" onclick="saveEditCategori(${expense.id})">Submit</button>
+      <button type="submit" class="btn btn-danger" onclick="deleteExpense(${expense.id})">Delete</button>
+    </form>
+    `
+  homeContent.innerHTML = expenseForm
+
+  document.querySelector('#content').appendChild(homeContent)
 }
 
 function deleteExpense(id){
@@ -142,7 +137,10 @@ function loadCreateExpenseForm(){
   homeContent.className = 'pt-5 px-5'
 
   let expenseForm = `
-    <button type="button" class="btn btn-outline-dark float-end" onclick="closeCreateExpenseForm()">Close</button>
+
+    <a href="/Dompetin" class="text-decoration-none">
+      <button type="button" class="btn btn-outline-dark float-end">Close</button>
+    </a>
     <form class="m-auto mt-5">
       <div class="mb-3">
         <label for="amount" class="form-label">Amount :</label>
@@ -173,18 +171,129 @@ function loadCreateExpenseForm(){
   document.querySelector('#content').appendChild(homeContent);
 }
 
-function closeCreateExpenseForm(){
+function listExpensesByCategory(categoryId) {
   const dataUser = loadData()
 
-  let mainContent = document.createElement("div")
-  mainContent.id = 'main-content'
-  mainContent.className = 'container flex-grow-1 overflow-auto'
+  document.querySelector('#main-content').innerHTML = ''
 
-  document.querySelector('#content').innerHTML = ''
-  document.querySelector('#content').appendChild(mainContent)
-
-  displayHomeContent(dataUser)
+  for (let i = 0; i < dataUser.expenses.length; i++) {
+    if (dataUser.expenses[i].categoryId === categoryId) {
+     let homeContent = document.createElement("div")
+     homeContent.innerHTML = `
+     <div class="card shadow-sm m-2 my-3" onclick="editExpense(${dataUser.expenses[i].id})">
+      <div class="card-body d-flex justify-content-between p-4">
+        <div> 
+          <h6 class="card-subtitle">${dataUser.expenses[i].note}</h6>
+          <p class="card-text">Rp. ${Intl.NumberFormat('id-ID').format(dataUser.expenses[i].amount)}</p>
+        </div>
+        <p class="card-text">${dataUser.expenses[i].date}</p>
+      </div>
+       `
+    document.querySelector('#main-content').appendChild(homeContent)
+    }
+  }
 }
+
+function listCategories() {
+  const dataUser = loadData()
+
+  if (!document.querySelector('#main-content')) {
+    let homeContent = document.createElement("div")
+    homeContent.id = 'main-content'
+    homeContent.className = 'container flex-grow-1 overflow-auto'
+
+    document.querySelector('#content').innerHTML = ''
+    document.querySelector('#content').appendChild(homeContent);
+    document.querySelector('#main-content').after(displayMainMenuCOnten())
+
+  }
+
+  document.querySelector('#main-content').innerHTML = '<h4 class="p-4 text-center">Daftar Kategori</h4>'
+
+  for (let i = 0; i < dataUser.categories.length; i++) {
+    let homeContent = document.createElement("div")
+    homeContent.innerHTML = `
+    <div class="card shadow-sm m-2 my-3" onclick="editCategoriForm(${dataUser.categories[i].id})">
+      <div class="card-body d-flex justify-content-between p-4">
+        <div> 
+          <h6 class="card-subtitle">${dataUser.categories[i].name}</h6>
+          </div>
+        <p class="card-text">Rp. ${Intl.NumberFormat('id-ID').format(dataUser.categories[i].budget)}</p>
+      </div>
+       `
+
+    document.querySelector('#main-content').appendChild(homeContent)
+  }
+}
+
+function editCategoriForm(id) {
+  const dataUser = loadData()
+  let category = ''
+  
+  for (let i = 0; i < dataUser.categories.length; i++) {
+    if (dataUser.categories[i].id === id) {
+      category = dataUser.categories[i]
+    }   
+  }
+
+  document.querySelector('#main-content').remove()
+  document.querySelector('#menu').remove()
+
+  let homeContent = document.createElement("div");
+  homeContent.className = 'pt-5 px-5'
+
+  let expenseForm = `
+    <button type="button" class="btn btn-outline-dark float-end" onclick="listCategories()">Close</button>
+    <form class="m-auto mt-5">
+      <div class="mb-3">
+        <label for="amount" class="form-label">Amount :</label>
+        <input type="number" class="form-control" id="amount" placeholder="100000" value="${category.budget}">
+      </div>
+      <button type="submit" class="btn btn-primary" onclick="saveEditCategori(${category.id})">Submit</button>
+    </form>
+    `
+  homeContent.innerHTML = expenseForm
+
+  document.querySelector('#content').appendChild(homeContent)
+}
+
+function saveEditCategori(id) {
+  const dataUser = loadData()
+  let categories = dataUser.categories
+
+  for (let i = 0; i < categories.length; i++) {
+    if (categories[i].id === id) {
+      categories[i].budget = Number(document.getElementById("amount").value) 
+    }
+  }
+
+  return saveData(dataUser)
+}
+
+function displayMainMenuCOnten() {
+  
+    /**
+     * Add main menu
+     */
+     let mainMenu = document.createElement("div")
+     mainMenu.id = 'menu'
+     mainMenu.className = 'border-top'
+ 
+     mainMenu.innerHTML = `
+       <ul class="list-group list-group-flush flex-row justify-content-around py-3 px-2">
+         <a href="/Dompetin" class="text-decoration-none">
+           <li class="list-group-item border rounded-2 p-3">Home</li>
+         </a>
+         <li class="list-group-item border rounded-2 p-3" onclick="loadCreateExpenseForm()">Tambah Pengeluaran</li>
+         <li class="list-group-item border rounded-2 p-3" onclick="listCategories()">Kategori</li>
+       </ul>`
+
+    return mainMenu
+ 
+}
+
+
+/** END EXPENSES */
 
 function displayHomeContent(dataUser){
 
@@ -207,21 +316,24 @@ function displayHomeContent(dataUser){
      */
     let homeCategoriesContent = document.createElement("div")
     homeCategoriesContent.id = 'category-card'
+    homeCategoriesContent.innerHTML += `<h5 class="mb-0 mx-2">Daftar Pengeluaran</h5>`
+    
     const filteredExpensesByCategory = filterExpensesByCategory(dataUser.expenses, dataUser.categories)
     
     if (filteredExpensesByCategory.length) {
       for (let i = 0; i < filteredExpensesByCategory.length; i++) {
         let exceed = false, percentage = 0
 
+
         if(filteredExpensesByCategory[i].budget > 0 && filteredExpensesByCategory[i].budget < filteredExpensesByCategory[i].amount){
           percentage = 100
           exceed = true
         } else {
-          percentage = filteredExpensesByCategory[i].budget / filteredExpensesByCategory[i].amount
+          percentage = Math.floor((filteredExpensesByCategory[i].amount / filteredExpensesByCategory[i].budget) * 100)
         }
 
         homeCategoriesContent.innerHTML += `
-        <div class="card shadow-sm m-2 my-4">
+        <div class="card shadow-sm m-2 my-4" onclick="listExpensesByCategory(${filteredExpensesByCategory[i].id})">
           <div class="card-body d-flex p-4">
             <div> 
               <h6 class="card-subtitle">${filteredExpensesByCategory[i].name}</h6>
@@ -229,7 +341,8 @@ function displayHomeContent(dataUser){
             </div>
             <div class="flex-grow-1 text-end my-auto">
               ${exceed ? `<small class="text-danger">exceed</small>` : `` }
-              <h5 class="card-title ${exceed ? `text-danger ` : `` }">Rp. ${Intl.NumberFormat('id-ID').format(filteredExpensesByCategory[i].amount)}</h5>
+              <h5 class="card-subtitle ${exceed ? `text-danger ` : `` }">Rp. ${Intl.NumberFormat('id-ID').format(filteredExpensesByCategory[i].amount)}</h5>
+              <small class="text-muted ${exceed ? `text-danger ` : `` }">dari Rp. ${Intl.NumberFormat('id-ID').format(filteredExpensesByCategory[i].budget)}</small>
             </div>
         </div>
         <div class="progress" style="height: 2px;">
@@ -240,25 +353,16 @@ function displayHomeContent(dataUser){
      }
     }
 
-    /**
-     * Add main menu
-     */
-    let mainMenu = document.createElement("div")
-    mainMenu.id = 'menu'
-    mainMenu.className = 'border-top'
-
-    mainMenu.innerHTML = `
-      <ul class="list-group list-group-flush flex-row justify-content-around py-3 px-2">
-        <li class="list-group-item border rounded-2 p-3">Home</li>
-        <li class="list-group-item border rounded-2 p-3" onclick="loadCreateExpenseForm()">Tambah Pengeluaran</li>
-        <li class="list-group-item border rounded-2 p-3">Kategori</li>
-      </ul>`
-
     document.querySelector('#main-content').appendChild(homeContent)
     document.querySelector('#main-content').appendChild(homeCategoriesContent)
-    document.querySelector('#main-content').after(mainMenu)
+    document.querySelector('#main-content').after(displayMainMenuCOnten())
 }
 
+
+function homeButton() {
+  document.querySelector('#main-content').innerHTML = ''
+  displayHomeContent()
+}
 
 
 /**
